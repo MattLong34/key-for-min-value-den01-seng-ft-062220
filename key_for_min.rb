b413fec1a178737942f40e832d05cca65d4ace1e
Matt Long <mdlong34@gmail.com>
2020-06-03 20:05:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# def key_for_min_value(name_hash)
-#   name_hash.min do |key, value|
-#     stuff
-#   end 
-# end
-
-
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
